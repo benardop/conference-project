f54fd788f36c92f8ca3f739c0cbd2dd767ea9f55
benardop <benard.oc.p@gmail.com>
2022-08-06 10:06:21 +0300

    Add all the correct image path in te index.js file

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,9 +23,9 @@ btnClose.addEventListener('click', () => {
 const speakers = document.querySelector('.speakers');
 
 const speakerObjData = {
-  profileImg: ['/assets/images/speaker_06.png', '/assets/images/speaker_01.png',
-    '/assets/images/speaker_02.png', '/assets/images/speaker_03.png',
-    '/assets/images/speaker_04.png', '/assets/images/speaker_05.png'],
+  profileImg: ['./assets/images/speaker_06.png', './assets/images/speaker_01.png',
+    './assets/images/speaker_02.png', './assets/images/speaker_03.png',
+    './assets/images/speaker_04.png', './assets/images/speaker_05.png'],
   name: ['Ryan Berkley', 'yohai benkler', 'Jeon Gil-nam', 'Ryan Merkley', 'Julia Leda', 'Layla Trettikov'],
   title: ['Ryan Berkley', 'Creative Commons CEO, Former Mozilla Foundation COO', 'Executive Director of the Wikimedia Foundation', '', 'President of Young Pirates of Europe', 'CEO of Creative Commons, ex COO of the Mozilla Foundation'],
   description: ['He led open source projects at the Mozilla Foundation and joined CC as CEO in 2014. He has been active in open movements such as open government and open source.', 'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.', 'Lila Tretikov is the Executive Director of the Wikimedia Foundation, the nonprofit organization that operates Wikipedia. Wikipedia is freely available in 290 languages and used by nearly half a billion people around the world every month.', 'Kilnam Chon helped bring the Internet to Asia and is an outspoken advocate for the open web and digital commons. In 2012, he was inducted into the inaugural class of the Internet Society\'s (ISOC) Internet Hall of Fame.', 'European ingetration, political democracy and participation of youth through online as her major condem, Reda\'s report outlining potential changes to EU copyright law was approved by the Parliament in July.', 'Ryan had been leading open-source projects at the Mozilla Foundation such as the open source movement.'],
